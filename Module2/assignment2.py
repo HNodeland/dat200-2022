d582afc71747b45312bb1d29797b33fb54fdbac3
HNodeland <haakon@nodeland.no>
2022-08-31 16:43:25 +0200

    x

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -5,11 +5,12 @@ class ArrayList:
     def __init__(self, startcapasity = 20):
         self.array = np.zeros(startcapasity, dtype=object)
         self.length = 0
+        self.startIndex = 0
 
     def __len__(self):
         return self.length
     
-    def expand(self, new_size=None):
+    def expand_left(self, new_size=None):
         #if no new size is specified, double the array size
         if new_size is None:
             new_size = len(self.array)*2
@@ -26,16 +27,31 @@ class ArrayList:
         
         #set the new array to be the current array
         self.array = new_array
+        self.startIndex += 1
     
+    def expand_right(self, new_size = None):
+        if new_size is None:
+            new_size = len(self.array)*2
+        
+        new_array = np.zeros(new_size, dtype = object)
+
+        old_array_len = len(self.array)
+        for i in range(old_array_len):
+            new_array
+
     def append_left(self, element):
         if self.length >= len(self.array):
-            self.expand()
+            self.expand_left()
         
         if self.length == 0:
             self.array[-1] = element
         else:
             self.array[-(self.length+1)] = element
         self.length += 1
+    
+      
+        
+        
 
 if __name__ == "__main__":
     list = ArrayList(2)
@@ -47,6 +63,5 @@ if __name__ == "__main__":
     list.append_left(3)
     list.append_left(4)
     list.append_left(5)
-    
     
     print(list.array)
