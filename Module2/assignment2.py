96e7421e1a1b72c52089655729a2d17dcacf66a6
HNodeland <haakon@nodeland.no>
2022-09-03 16:38:20 +0200

    Lagt til kommentarer om kjøretid

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -9,16 +9,22 @@ class ArrayList:
         self.buffer_left = int(startcapasity/2)
         self.size = startcapasity
 
+    #Kjøretid O(1)
     def __len__(self):
         return self.length
     
-    #
+    #Kjøretid best case O(1)
+    #Kjøretid worst case O(n), når lista må utvides, og alle elementer må flyttes
+    #Kjøretid amortized O(1)
     def append_right(self, element):
         if self.buffer_left + self.length >= len(self.array):
             self.expand_right()
         self.array[self.buffer_left + self.length] = element
         self.length += 1
 
+    #Kjøretid best case O(1)
+    #Kjøretid worst case O(n), når lista må utvides, og alle elementer må flyttes
+    #Kjøretid amortized O(1)
     def append_left(self, element):
         if self.buffer_left == 0:
             self.expand_left()
@@ -26,6 +32,7 @@ class ArrayList:
         self.length += 1
         self.buffer_left -= 1
 
+    #Kjøretid O(n)
     def expand_right(self, new_size = None):
         if new_size is None:
             new_size = len(self.array)*2
@@ -34,6 +41,7 @@ class ArrayList:
             new_array[i] = self.array[i]
         self.array = new_array
 
+    #Kjøretid O(n)
     def expand_left(self, new_size = None):
         if new_size is None:
             new_size = len(self.array)*2
@@ -43,15 +51,19 @@ class ArrayList:
             new_array[self.buffer_left + i] = self.array[i]
         self.array = new_array
 
+    #Kjøretid O(1)
     def pop_left(self):
         self.array[self.buffer_left] = 0
         self.buffer_left += 1
         self.length -= 1
 
+    #Kjøretid O(1)
     def pop_right(self):
         self.array[self.buffer_left + self.length -1] = 0
         self.length -= 1
 
+    #Kjøretid O(1) best case, get på første element i lista
+    #Kjøretid O(n) worst case, når vi leter gjennom hele arrayet 
     def get(self, index):
         print(self.array[self.buffer_left + index])
 
